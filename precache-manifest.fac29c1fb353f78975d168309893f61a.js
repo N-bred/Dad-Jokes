667c5d73bdf982254581a5f4bc235542c35b66c8
N-bred <thehardyboiz1@gmail.com>
2019-07-15 00:26:15 -0500

    Ready to deploy

---
+++ precache-manifest.fac29c1fb353f78975d168309893f61a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1734bbb90d73eb513748130384f67bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c0206a38ac324fa3a9e",
+    "url": "/static/css/main.6caa960d.chunk.css"
+  },
+  {
+    "revision": "5d73d5f1487e524b2d99",
+    "url": "/static/js/2.13caf74f.chunk.js"
+  },
+  {
+    "revision": "7c0206a38ac324fa3a9e",
+    "url": "/static/js/main.c1e01ea7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1241b82b7154f21a6e807e72b215051e",
+    "url": "/static/media/001-emoji.1241b82b.svg"
+  },
+  {
+    "revision": "a8b7f5f6c0153812eb82c9ecb2806b1c",
+    "url": "/static/media/002-emoji-1.a8b7f5f6.svg"
+  },
+  {
+    "revision": "d310fc7ac37137030c6db3bec6c3b42d",
+    "url": "/static/media/003-emoji-2.d310fc7a.svg"
+  },
+  {
+    "revision": "acfd2d8c56912acf6fafecd4e9186cbb",
+    "url": "/static/media/004-sad.acfd2d8c.svg"
+  },
+  {
+    "revision": "b4a99b89f8bf0e4f3a1f234de6811f8b",
+    "url": "/static/media/005-emoji-3.b4a99b89.svg"
+  },
+  {
+    "revision": "5cf827db258adaec75fc23046273b144",
+    "url": "/static/media/006-emoji-4.5cf827db.svg"
+  },
+  {
+    "revision": "bce964bfd9a9778f5db7b988d7aab8d0",
+    "url": "/static/media/007-emoji-5.bce964bf.svg"
+  },
+  {
+    "revision": "4cea71af04015722386d5d060af26338",
+    "url": "/static/media/008-emoji-6.4cea71af.svg"
+  },
+  {
+    "revision": "45045e34e66aa385bc0192a50e21fb7a",
+    "url": "/static/media/dislike-thumb.45045e34.svg"
+  },
+  {
+    "revision": "9d0356f606ef6208077e638924331d5c",
+    "url": "/static/media/like.9d0356f6.svg"
+  }
+]);
